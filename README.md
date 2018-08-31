620fb9fa1814ac193f607e08be3aebc5e1656074
Joseph <Joseph@youzeek.com>
2018-08-31 17:38:50 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,118 @@
+# appdrag-datasource-filemaker
+
+<br/>
+
+Node.js Wrapper for Filemaker API 17+ created and maintained by AppDrag
+
+[AppDrag](https://appdrag.com "AppDrag") is a Cloud CMS & Backend made for web professionals and hosted on Amazon AWS. With AppDrag produce 5 times faster websites, blog e-commerce, Databases & API's.
+
+
+### Installing with NPM
+Simply type the following into a terminal window:
+
+`$ npm install appdrag-datasource-filemaker`
+<br/><br/>
+
+### Installing with CloudBackend
+If your are inside a cloud function in CloudBackend, click on the "Open library manager" button then search for "appdrag-datasource-filemaker", click on the "+" button then save.
+
+![CloudBackend API Dashboard](https://cf.appdrag.com/cloudbackend/assets/AppDrag-Cloudbackend-API-NPM-Manager.jpg "CloudBackend NPM Manager")
+
+<br/><br/>
+
+## Initialization
+```
+var filemaker = require('appdrag-datasource-filemaker');
+filemaker.init('https://yourdomain.com', 'dbname', 'username', 'password');
+```
+
+<br/>
+
+## RECORDS
+### List Records (layoutName, rowLimit) 
+```
+var records = JSON.parse(await filemaker.List("your_layout_name", 50));
+callback(null, filemaker.ConvertToDataSource(records) );
+```
+
+### Find Records (layoutName, query)
+```
+var dataQuery = {
+    "query": [{
+        "Nom": "=Jobs"
+    }],
+    "sort": [
+        {"fieldName": "Prenom", "sortOrder": "ascend"},
+        {"fieldName": "Nom","sortOrder": "ascend"}
+    ]
+};
+var records = await filemaker.find("Api_Contact", dataQuery);
+callback(null, filemaker.ConvertToDataSource(records));
+```
+
+### Get Record by id (layoutName, recordId) 
+```
+var records = JSON.parse(await filemaker.Get("Api_Contact", 1));
+callback(null, filemaker.ConvertToDataSource(records) );
+```
+
+
+### Create Record (layoutName, data) 
+```
+var data = {
+    "fieldData": {
+        "Prenom": "Jean",
+        "Nom": "Dupont"
+    }
+};
+var response = await filemaker.Create("Api_Contact", data);
+callback(null, response);
+```
+
+### Edit Record (layoutName, data) 
+```
+var data = {
+    "fieldData": {
+        "Titre": "Professeur",
+        "Societe": "IBM", 
+        "Nom": "Dupond2"
+    }
+};
+var response = await filemaker.Edit("Api_Contact", 8, data);
+callback(null, response);
+```
+
+### Delete Record (layoutName, recordId) 
+```
+var records = JSON.parse(await filemaker.Delete("Api_Contact", 1));
+callback(null, response); 
+```
+
+
+### Execute Script (layoutName, query)
+```
+var query = {
+    "fieldData": {
+    },
+    "script.prerequest": "YourScriptName1",
+    "script.presort": "YourScriptName2",
+    "script": "YourScriptName3"
+};
+var response = await filemaker.Execute("Api_Contact", query);
+callback(null, response);
+```
+
+
+### GetToken()
+```
+var token = await filemaker.GetToken();
+callback(null, response);
+```
+
+
+<br/>
+
+
+
+# Support
+Post your issues or suggestions here on Github
---
+++ index.js
@@ -0,0 +1,243 @@
+var request = require('request');
+
+var rootUrl = 'https://xxxxxxxx.com';
+var dbName = 'DBNameHere';
+var username = 'yourUserName';
+var password = 'yourPassword';
+var filemakerAPIUrl = rootUrl + "/fmi/data/v1/databases/" + dbName;
+
+exports.init = function(_rootUrl, _dbname, _username, _password) {
+  rootUrl = _rootUrl;
+  username = _username;
+  password = _password;
+  filemakerAPIUrl = _rootUrl + "/fmi/data/v1/databases/" + _dbname;       
+}
+
+exports.fileTextWrite = function(filekey, content) {
+
+  return new Promise((resolve, reject) => {
+    var postParameters = {
+        "command" : "WriteTextFile",
+        "APIKey" : APIKey,
+        "appID" : appID,
+        "filekey" : filekey,
+        "content" : content
+    };
+
+    request.post(
+    {
+        url:APIUrl,
+        form: postParameters
+      }, function(err,httpResponse,body){
+        return resolve(body);
+      }
+    );
+  });
+}
+
+
+exports.fileTextWrite = function(filekey, content) {
+
+  return new Promise((resolve, reject) => {
+    var postParameters = {
+        "command" : "WriteTextFile",
+        "APIKey" : APIKey,
+        "appID" : appID,
+        "filekey" : filekey,
+        "content" : content
+    };
+
+    request.post(
+    {
+        url:APIUrl,
+        form: postParameters
+      }, function(err,httpResponse,body){
+        return resolve(body);
+      }
+    );
+  });
+}
+
+
+function GetToken() {
+    return new Promise(function(resolve, reject) {
+        request.post({
+            url: filemakerAPIUrl + "/sessions",
+            headers: {
+                'content-type': 'application/json',
+                'Authorization': "Basic " + new Buffer(username + ":" + password).toString("base64")
+            },
+            method: 'POST'
+        }, function(error, response, body) {
+
+            //callback(null, body); // Return response from the server
+            //return; // stop execution
+
+            var token = "";
+            try {
+                token = JSON.parse(body).response.token;
+                console.log("token: " + token);
+                return resolve(token);
+            } catch (ex) {
+                return resolve(ex + " - " + body);
+            }
+
+        });
+
+    });
+} 
+exports.GetToken = GetToken;
+
+
+
+async function ListRecords(layoutName, limit) {
+    var token = await GetToken();
+    return new Promise(function(resolve, reject) {
+        request.get({
+            url: filemakerAPIUrl + "/layouts/" + layoutName + "/records?_limit=" + limit,
+            headers: {
+                'Authorization': "Bearer " + token
+            },
+            method: 'GET'
+        }, function(error, response, body) {
+            return resolve(body);
+        });
+
+    });
+}
+exports.List = ListRecords;
+
+
+async function FindRecords(layoutName, data) {
+    var token = await GetToken();
+    return new Promise(function(resolve, reject) {
+        request.post({
+            url: filemakerAPIUrl + "/layouts/" + layoutName + "/_find",
+            headers: {
+                'Authorization': "Bearer " + token,
+                'Content-Type': 'application/json'
+            },
+            method: 'POST',
+            json: data
+        }, function(error, response, body) {
+            return resolve(body);
+        });
+
+    });
+}
+exports.Find = FindRecords;
+
+async function GetRecord(layoutName, id) {
+    var token = await GetToken();
+    return new Promise(function(resolve, reject) {
+        request.get({
+            url: filemakerAPIUrl + "/layouts/" + layoutName + "/records/" + id,
+            headers: {
+                'Authorization': "Bearer " + token
+            },
+            method: 'GET'
+        }, function(error, response, body) {
+            return resolve(body);
+        });
+
+    });
+}
+exports.Get = GetRecord;
+
+async function CreateRecord(layoutName, data) {
+    var token = await GetToken();
+    return new Promise(function(resolve, reject) {
+        request.post({
+            url: filemakerAPIUrl + "/layouts/" + layoutName + "/records",
+            headers: {
+                'Authorization': "Bearer " + token,
+                'Content-Type': 'application/json'
+            },
+            method: 'POST',
+            json: data
+        }, function(error, response, body) {
+            return resolve(body);
+        });
+
+    });
+}
+exports.Create = CreateRecord;
+
+async function DeleteRecord(layoutName, id) {
+    var token = await GetToken();
+    return new Promise(function(resolve, reject) {
+        request.delete({
+            url: filemakerAPIUrl + "/layouts/" + layoutName + "/records/" + id,
+            headers: {
+                'Authorization': "Bearer " + token,
+                'Content-Type': 'application/json'
+            },
+            method: 'DELETE'
+        }, function(error, response, body) {
+            return resolve(body);
+        });
+
+    });
+}
+exports.Delete = DeleteRecord;
+
+async function EditRecord(layoutName, id, data) {
+    var token = await GetToken();
+    return new Promise(function(resolve, reject) {
+        request.patch({
+            url: filemakerAPIUrl + "/layouts/" + layoutName + "/records/" + id,
+            headers: {
+                'Authorization': "Bearer " + token,
+                'Content-Type': 'application/json'
+            },
+            method: 'PATCH',
+            json: data
+        }, function(error, response, body) {
+            return resolve(body);
+        });
+
+    });
+}
+exports.Edit = EditRecord;
+
+async function ExecuteScript(layoutName, data) {
+    var token = await GetToken();
+    return new Promise(function(resolve, reject) {
+        request.post({
+            url: filemakerAPIUrl + "/layouts/" + layoutName + "/records",
+            headers: {
+                'Authorization': "Bearer " + token,
+                'Content-Type': 'application/json'
+            },
+            method: 'POST',
+            json: data
+        }, function(error, response, body) {
+            return resolve(body);
+        });
+
+    });
+}
+exports.Execute = ExecuteScript;
+
+function ConvertToDataSource(records) {
+    var datasource = new Object();
+    datasource.Table = [];
+
+    var rows = records.response.data;
+    for (i = 0; i < rows.length; i++) {
+        var newLine = new Object();
+        newLine.recordId = rows[i].recordId;
+        newLine.modId = rows[i].modId;
+        for (key in rows[i].fieldData) {
+            newLine[key] = rows[i].fieldData[key];
+        }
+        newLine.portalData = rows[i].portalData;
+
+        datasource.Table.push(newLine);
+
+    }
+
+    return datasource;
+}
+exports.ConvertToDataSource = ConvertToDataSource;
+
